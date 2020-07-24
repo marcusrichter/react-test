85b3059ba901419aaae6bc6fd109b721dc795442
unknown <marcus.richter@gmx.net>
2020-07-24 18:53:47 +0200

    Dropdown Submenu

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,9 @@ function App() {
     <div className="container-fluid jumbo-teaser">
       <div className="row justify-content-center">
         <div className="col-12 d-flex">
-          <div className="mx-auto my-auto">
+          <div className="mx-auto my-auto text-center">
             <h2>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</h2>
+            <button type="button" className="btn btn-yellow-filled mt-4">Primary</button>
           </div>
         </div>      
       </div>    
--- src/pageElements/Header.tsx
+++ src/pageElements/Header.tsx
@@ -5,6 +5,7 @@ import HamburgerMenu from './HamburgerMenu';
 
 export default () => {
     const [headerMinimized, setHeaderMinimized] = useState(false);
+    const [subMenuActivated, setSubmenuActivated] = useState<string|null>(null);
 
     useEffect(() => {    
       window.addEventListener('scroll', function(e) {
@@ -21,8 +22,18 @@ export default () => {
         </a>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-right d-none d-l-flex d-xl-flex">
-            <li className="navbar-nav">
-              <a href="#" className="nav-link">Users</a>
+            <li className="navbar-nav dropdown">
+              <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"
+              onMouseEnter={() => setSubmenuActivated('menu1')}
+              onMouseLeave={() => setSubmenuActivated(null)}
+              >Menu 1</a>
+              <ul className={clsx('dropdown-menu', subMenuActivated === 'menu1' && 'show')}>
+                <li className="text-center"><a href="#">Lorem ipsum dolor</a></li>
+                <li className="text-center"><a href="#">justo duo dolores</a></li>
+                <li className="text-center"><a href="#">takimata sanctus est</a></li>
+                <li className="text-center"><a href="#">sit amet</a></li>
+                <li className="text-center"><a href="#">sanctus est</a></li>
+              </ul>
             </li>
             <li className="navbar-nav">
               <a href="#" className="nav-link">Users</a>
