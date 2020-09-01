8eba386d14afb7751710294f28f3217ac5459a04
unknown <marcus.richter@gmx.net>
2020-09-01 16:54:19 +0200

    [FEAURE] implement router

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,8 @@ import './scss/app.scss';
 import Header from './pageElements/Header';
 import SideNav from './pageElements/SideNav';
 import PageContext from "./contexts/PageContext";
-import JumboTeaser from './pageElements/Containers/JumboTeaser';
-import GreyContainer from './pageElements/Containers/GreyContainer';
-import WhiteContainer from './pageElements/Containers/WhiteContainer';
-import TextCard from './pageElements/Cards/TextCard';
-import ImageRolloverCard from './pageElements/Cards/ImageRolloverCard';
-import Button from './pageElements/Buttons/Button';
+import { Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
   const [sideMenuActivated, setSideMenuActivated] = useState<boolean>(false);
@@ -26,76 +22,9 @@ function App() {
   return <PageContext.Provider value={{sideMenuActivated, setSideMenuActivated}}> 
     <Header></Header>
     <SideNav></SideNav>
-    <JumboTeaser backgroundImage="//picsum.photos/id/213/1000/800">
-      <div className="col-12 d-flex">
-        <div className="mx-auto my-auto text-center">
-          <h2 className="mb-4" data-aos="fade-down" data-aos-duration="1000">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</h2>
-          <h3 className="mb-4" data-aos="fade-left" data-aos-duration="1000">sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</h3>
-          <Button type="yellowFilled" className="mt-4" onClick={() => alert('x')}>
-              Mehr anzeigen
-          </Button>            
-        </div>
-      </div>  
-    </JumboTeaser>
-    <GreyContainer>
-      <div className="row justify-content-around pt-5">  
-        <TextCard title="Titel Alpha" subTitle="Thema A" buttonText="mehr">
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </TextCard>
-        <TextCard title="Titel Beta" subTitle="Thema B" buttonText="mehr">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </TextCard>
-        <TextCard title="Titel Gamma" subTitle="Sonstiges" buttonText="mehr">
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        </TextCard>
-       </div> 
-    </GreyContainer>
-
-    <WhiteContainer>
-      <div className="mx-auto text-center">
-        <h2 className="mb-4" data-aos="fade-down" data-aos-duration="1000">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</h2>
-        <h3 className="mb-4" data-aos="fade-left" data-aos-duration="1000">sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</h3>
-
-        <div className="row justify-content-around pt-5 pb-5">  
-            <ImageRolloverCard 
-              teaserText="At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-              title="Lorem Ipsum"
-              image="//picsum.photos/280/180"
-              >
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </ImageRolloverCard>
-            <ImageRolloverCard 
-              teaserText="At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-              title="Lorem Ipsum"
-              image="//picsum.photos/280/180"
-              >
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </ImageRolloverCard>
-            <ImageRolloverCard 
-              teaserText="At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-              title="Lorem Ipsum"
-              image="//picsum.photos/280/180"
-              >
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </ImageRolloverCard>
-        </div>
-      </div> 
-    </WhiteContainer>
-
-
-    <main role="main">
-
-      <section className="jumbotron text-center">
-        <div className="container">
-          <h1 className="jumbotron-heading">Album example</h1>
-          <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-          <p>
-            <a href="#" className="btn btn-primary my-2">Main call to action</a>
-            <a href="#" className="btn btn-secondary my-2">Secondary action</a>
-          </p>
-        </div>
-      </section>
-    </main>
+    <Switch>
+        <Route path="/" component={Home} exact />
+    </Switch>
 
     <footer className="text-muted">
       <div className="container">
--- src/pageElements/Cards/ImageRolloverCard.tsx
+++ src/pageElements/Cards/ImageRolloverCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props: any) => {
     return <div className="card col-sm-12 col-md-12 col-lg-3 mb-3 image-rollover-card">
-    <img className="card-img-top" src="{props.image}"/>
+    <img className="card-img-top" src={props.image}/>
     <div className="card-body">                
         <h5 className="card-title" data-aos="fade-left" data-aos-duration="1000">
             {props.title}
