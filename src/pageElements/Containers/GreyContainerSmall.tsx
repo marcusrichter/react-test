d75dbf780ba44946b9f775c4dfd7925cd2ff2a9a
unknown <marcus.richter@gmx.net>
2020-09-18 17:12:32 +0200

    [feature] formular work in progress

---
+++ src/pageElements/Containers/GreyContainerSmall.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default (props: any) => <div className="container grey-container">    
+        {props.children}    
+</div>;
--- src/pages/Interactive/FormularPage.tsx
+++ src/pages/Interactive/FormularPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import WhiteContainerSmall from './../../pageElements/Containers/WhiteContainerSmall';
+import GreyContainer from './../../pageElements/Containers/GreyContainer';
 import { useForm } from 'react-hook-form';
 import { FormControl, FormControlLabel, FormGroup, FormLabel, InputLabel, MenuItem, Select, TextField, Checkbox, FormHelperText, RadioGroup, Radio } from '@material-ui/core';
 import Button from '../../pageElements/Buttons/Button';
@@ -12,52 +12,60 @@ export default () => {
     };
 
     return <>
-    <WhiteContainerSmall>
-        <div className="row">
-            <div className="mx-auto text-center">
-                <h2 className="mb-4" data-aos="fade-down" data-aos-duration="1000">Formular Test</h2>
-                <h3 className="mb-4" data-aos="fade-left" data-aos-duration="1000">sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</h3>
-            </div>    
-        </div>
-        <div className="row mt-5 mb-5"></div>
-        <div className="row">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <TextField label="Name" type="text" name="name" value="" data-validators="isRequired,isAlpha" />
-                <fieldset>
-                    <FormControl>
-                        {/* form label is required here to perform default validations */}
-                        <FormLabel component="legend">I love React material UI form</FormLabel>
-                        <FormGroup>
-                        <FormControlLabel control={<Checkbox value='yes' />}
-                            label='I love React material UI form'/>
-                        </FormGroup>
-                    </FormControl>
-        
-                    <FormControl required>
-                    <InputLabel>Age</InputLabel>
-                    <Select value="" name="age">
-                        <MenuItem value=""><em>Please select your age ...</em></MenuItem>
-                        <MenuItem value={10}>Teens</MenuItem>
-                        <MenuItem value={20}>Twenties</MenuItem>
-                        <MenuItem value={30}>Thirties</MenuItem>
-                        <MenuItem value="40+">Fourties +</MenuItem>
-                    </Select>
-                    <FormHelperText>Some important helper text</FormHelperText>
-                    </FormControl> 
-                </fieldset>
-                
-                <FormControl>
-                    <FormLabel component="legend">Gender</FormLabel>
-                    <RadioGroup aria-label="Gender" name="gender"  value="male">
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" /> 
-                    </RadioGroup>
-                </FormControl>
-                
-                <Button variant="raised" type="reset">Reset</Button>
-                <Button variant="raised" type="submit">Submit</Button>
-            </form>
-        </div>
-    </WhiteContainerSmall>    
-</>
+        <GreyContainer>
+            <div className="row">
+                <div className="mx-auto text-center">
+                    <h2 className="mb-4 mt-5" data-aos="fade-down" data-aos-duration="1000">Formular Test</h2>
+                    <h3 className="mb-4" data-aos="fade-left" data-aos-duration="1000">sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</h3>
+                </div>
+            </div>
+            <div className="row mt-5 mb-5"></div>
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <TextField label="Vorname" type="text" name="firstname" data-validators="isRequired,isAlpha" fullWidth={true}/>
+                        <TextField label="Nachname" type="text" name="lastname" data-validators="isRequired,isAlpha" fullWidth={true}/>
+                        <TextField label="EMail" type="text" name="email" data-validators="isRequired,isAlpha" fullWidth={true}/>
+                        <FormControl required  fullWidth={true}>
+                            <InputLabel>Age</InputLabel>
+                            <Select name="Alter">
+                                <MenuItem value=""><em>Bitte das Alter auswählen ...</em></MenuItem>
+                                <MenuItem value={18}>18 - 25</MenuItem>
+                                <MenuItem value={26}>26 - 35</MenuItem>
+                                <MenuItem value={36}>36+</MenuItem>                                
+                            </Select>
+                            <FormHelperText>Mindestalter 18</FormHelperText>
+                        </FormControl>
+
+                        <fieldset>
+                            <FormControl fullWidth={true}>
+                                {/* form label is required here to perform default validations */}
+                                <FormLabel component="legend">I love React material UI form</FormLabel>
+                                <FormGroup>
+                                    <FormControlLabel control={<Checkbox value='yes' />}
+                                        label='I love React material UI form' />
+                                </FormGroup>
+                            </FormControl>
+
+
+                        </fieldset>
+
+                        <FormControl>
+                            <FormLabel component="legend">Gender</FormLabel>
+                            <RadioGroup aria-label="Gender" name="gender" value="male">
+                                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                <FormControlLabel value="male" control={<Radio />} label="Male" />
+                            </RadioGroup>
+                        </FormControl>
+
+                        <br></br>
+
+                        <Button variant="raised" type="reset">Reset</Button>
+                        <Button variant="raised" type="submit">Submit</Button>
+                    </form>
+                </div>
+
+            </div>
+        </GreyContainer>
+    </>
 };
