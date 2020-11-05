47156d330e7fb365958ca6818354f3109fd78fe9
unknown <marcus.richter@gmx.net>
2020-11-05 16:38:03 +0100

    [FEATURE]  finish parallax page

--- src/pages/LayoutTests/ParallaxPage.tsx
+++ src/pages/LayoutTests/ParallaxPage.tsx
@@ -3,15 +3,86 @@ import WhiteContainerSmall from '../../pageElements/Containers/WhiteContainerSma
 import JumboTeaser from '../../pageElements/Containers/JumboTeaser';
 import Button from '../../pageElements/Buttons/Button';
 import { Element, scroller } from 'react-scroll';
+import LightGreyContainer from '../../pageElements/Containers/LightGreyContainer';
+import Plx from 'react-plx';
 
 const scrollForMore = () => {
     scroller.scrollTo('moreScrollToElement', {
-      duration: 1500,
-      delay: 100,
-      smooth: true,            
-      offset: -100, 
+        duration: 1500,
+        delay: 100,
+        smooth: true,            
+        offset: -100, 
     });
-  };
+};
+
+const parallaxScale = [
+{
+    start: 0,
+    end: 800,
+        properties: [
+            {
+                startValue: 1,
+                endValue: 1.4,
+                property: 'scale',
+            },
+        ],
+    },
+];
+
+const parallaxUnScaleAndGoTop = [
+{
+    start: 0,
+    end: 400,
+    easing: 'easeOutSine',
+    properties: [
+            {
+                startValue: 1.5,
+                endValue: 1.0,
+                property: 'scale',
+            },
+            {
+                startValue: 400,
+                endValue: 0,
+                property: 'translateY',
+            },
+        ],
+    },
+];
+
+const iconParallax = (index: number): Array<any> => 
+[
+    {
+        start: 0,
+        end: 400 + 300 * index,
+        easing: 'easeIn',
+        properties: [
+            {
+                startValue: 1000,
+                endValue: 0,
+                property: 'translateX',
+            },
+        ],
+    },
+];
+
+const listEntryTextParallax = (index: number): Array<any> => 
+[
+    {
+        start: 0,
+        end: 1300 + 280 * index,
+        easing: 'ease',
+        properties: [
+            {
+                startValue: -3.5,
+                endValue: 1,
+                property: 'opacity'
+            },
+        ],
+    },
+];
+
+
+
 
 export default () => <>
     <JumboTeaser backgroundImage="//picsum.photos/1000/1600" parallax={true}>
@@ -25,82 +96,52 @@ export default () => <>
         </div>
         </div>  
     </JumboTeaser>
+    <LightGreyContainer>
+        <div className="row pt-5 pb-5">
+            <div className="mx-auto text-center">
+            <Plx parallaxData={ parallaxScale }>
+                <h2 className="mb-4 mt-3">Parrallax Plx Test</h2>                             
+            </Plx>   
+
+            <Plx parallaxData={ parallaxUnScaleAndGoTop }>
+                <h3 className="mb-4">sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</h3>
+            </Plx>
+            </div>
+        </div>
+    </LightGreyContainer>
     <WhiteContainerSmall>
         <div className="row mt-5 mb-5"></div>
-        <div className="row">
-            <Element name="moreScrollToElement"></Element>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
-            <p>Lsaddsds dasasdmd adsadd adad adssd dad d d ad asd sd </p>
+        <div className="row pb-5">
+            <div className="col-12">
+                <ol className="list fancy-list-one mx-auto">
+                    <li>
+                        <Plx parallaxData={ iconParallax(1) }>
+                            <h2 id="x1">Entry one</h2>
+                        </Plx>                    
+                        <Plx parallaxData={ listEntryTextParallax(1) }>
+                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio ad corporis, laboriosam unde provident, architecto tenetur ea odio debitis delectus explicabo eum obcaecati vitae facere iusto laborum consequuntur neque.</span>
+                        </Plx>
+                    </li>
+                    <li>
+                        <Plx parallaxData={ iconParallax(2) }>
+                            <h2 id="x1">Entry two2</h2>
+                        </Plx>    
+                        <Plx parallaxData={ listEntryTextParallax(2) }>
+                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio ad corporis, laboriosam unde provident, architecto tenetur ea odio debitis delectus explicabo eum obcaecati vitae facere iusto laborum consequuntur neque.</span>
+                        </Plx>    
+                    </li>
+                    <li>
+                        <Plx parallaxData={ iconParallax(3) }>
+                            <h2 id="x1">Entry three</h2>
+                        </Plx>    
+                        <Plx parallaxData={ listEntryTextParallax(3) }>
+                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio ad corporis, laboriosam unde provident, architecto tenetur ea odio debitis delectus explicabo eum obcaecati vitae facere iusto laborum consequuntur neque.</span>
+                        </Plx>
+                    </li>
+                </ol>                
+            </div>
         </div>
+        <div className="row pb-5"></div>
+        <div className="row pb-5"></div>
     </WhiteContainerSmall>    
 </>;
