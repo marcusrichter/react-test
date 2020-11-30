f2b033e682978a0a22125daf57883d5cd6f6228d
mrichter <marcus.richter@interlutions.de>
2020-11-30 17:30:01 +0100

    [FEATURE] route planner work in progress

---
+++ src/entities/IFormRoutePlanner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface IFormRoutePlanner {
+    fromStreet: string;
+    fromCity: string;
+    toStreet: string;
+    toCity: string;
+    goingBY: number;
+}
+
+export default IFormRoutePlanner;
+
--- src/pages/Interactive/RoutePlannerPage.tsx
+++ src/pages/Interactive/RoutePlannerPage.tsx
@@ -1,22 +1,34 @@
 import React, {useState, useRef, useEffect} from "react";
-import GreyContainer from "../../pageElements/Containers/GreyContainer";
 import WhiteContainerSmall from "../../pageElements/Containers/WhiteContainerSmall";
 import Map from '../../pageElements/Geo/Map';
 import JumboTeaser from "../../pageElements/Containers/JumboTeaser";
+import {useForm} from "react-hook-form";
+import {Element, scroller} from "react-scroll";
+import {TextField} from "@material-ui/core";
+import Button from "../../pageElements/Buttons/Button";
 
 export default () => {
+    const { register, handleSubmit, errors, control } = useForm();
+
+    const onSubmit = (data: any) => {
+        scroller.scrollTo('mapScrollToElement', {
+            duration: 1500,
+            delay: 100,
+            smooth: true,
+            offset: 50,
+        });
+    };
 
     return <>
-        <JumboTeaser backgroundImage="//picsum.photos/id/213/1000/800">
+        <JumboTeaser backgroundImage="//picsum.photos/id/1067/1000/800">
             <div className="col">
                 <div className="row">
                     <div className="col-md-8 offset-md-2 text-left pt-5 mt-5">
                         <div className="row row-mt-15em">
-                            <div className="col-md-7 mt-text animate-box fadeInUp animated-fast pr-5 pt-5" data-animate-effect="fadeInUp">
+                            <div className="col-md-7 mt-text pr-5 pt-5" data-aos="fade-down">
                                 <h1>Jetzt den kürzesten Weg finden oder eine Tour planen!</h1>
                             </div>
-                            <div className="col-md-4 col-md-push-1 animate-box fadeInRight animated-fast"
-                                 data-animate-effect="fadeInRight">
+                            <div className="col-md-4 col-md-push-1" data-aos="fade-left">
                                 <div className="search-box">
                                     <div className="tab">
 
@@ -27,31 +39,31 @@ export default () => {
                                                     <div className="row form-group">
                                                         <div className="col-md-12">
                                                             <label htmlFor="route_fromStreet" className="required">Start Strasse</label>
-                                                            <input type="text" id="route_fromStreet" name="route[fromStreet]" required className="form-control"/>
+                                                            <input type="text" name="fromStreet" required className="form-control" data-validators="isRequired,isAlpha" ref={register}/>
                                                         </div>
                                                     </div>
                                                     <div className="row form-group">
                                                         <div className="col-md-12">
                                                             <label htmlFor="route_fromCity" className="required">Start Stadt</label>
-                                                            <input type="text" id="route_fromCity" name="route[fromCity]" required className="form-control"/>
+                                                            <input type="text" name="fromCity" required className="form-control" data-validators="isRequired,isAlpha" ref={register}/>
                                                         </div>
                                                     </div>
                                                     <div className="row form-group">
                                                         <div className="col-md-12">
                                                             <label htmlFor="route_toStreet" className="required">Ziel Strasse</label>
-                                                            <input type="text" id="route_toStreet" name="route[toStreet]" required className="form-control"/>
+                                                            <input type="text" name="toStreet" required className="form-control" data-validators="isRequired,isAlpha" ref={register}/>
                                                         </div>
                                                     </div>
                                                     <div className="row form-group">
                                                         <div className="col-md-12">
                                                             <label htmlFor="route_toCity" className="required">Ziel Stadt</label>
-                                                            <input type="text" id="route_toCity" name="route[toCity]" required className="form-control"/>
+                                                            <input type="text" name="toCity" required className="form-control" data-validators="isRequired,isAlpha" ref={register}/>
                                                         </div>
                                                     </div>
                                                     <div className="row form-group">
                                                         <div className="col-md-12">
                                                             <label htmlFor="route_goingBy" className="required">Unterwegs mit</label>
-                                                            <select id="route_goingby" name="route[goingBy]" className="form-control">
+                                                            <select name="goingBy" className="form-control" data-validators="isRequired,isAlpha" ref={register}>
                                                                 <option value="driving-car">Auto</option>
                                                                 <option value="driving-hgv">LKW</option>
                                                                 <option value="cycling-regular">Fahrrad</option>
@@ -67,8 +79,7 @@ export default () => {
 
                                                     <div className="row form-group">
                                                         <div className="col-md-12">
-                                                            <button className="btn btn-grey-filled btn-block">Route Finden</button>
-
+                                                            <button className="btn btn-grey-filled btn-block mt-3">Route Finden</button>
                                                         </div>
                                                     </div>
                                                 </form>
@@ -85,6 +96,7 @@ export default () => {
             </div>
         </JumboTeaser>
         <WhiteContainerSmall>
+            <Element name="mapScrollToElement"></Element>
             <div className="row">
                 <Map/>
             </div>
